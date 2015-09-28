975544e08c2d8605f88991378b11069610e3ca99
Matthew Page <matthewpagecs@gmail.com>
2015-09-28 14:27:35 -0400

    Added better documentation for astar function

--- js/utilities.js
+++ js/utilities.js
@@ -1,6 +1,8 @@
 /**
   * Finds the shortest path from start to end using the A-Star algorithm.
   * grid: defined as in snakeAI.js
+  * startX, startY: The head of the snake
+  * endX, endY: The food location
   * returns: 0..3 depending on where the first move towards the goal from the start is. View snakeAI.js for what 0..3 correspond to.
   */
 function astar(grid, startX, startY, endX, endY) {
