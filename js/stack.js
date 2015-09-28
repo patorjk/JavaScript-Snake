4c66ab0d2bff37dd99da5214b7eb3019de9190f5
Matthew Page <matthewpagecs@gmail.com>
2015-09-28 14:26:59 -0400

    Added better documentation for stack.js

--- js/stack.js
+++ js/stack.js
@@ -1,6 +1,9 @@
 /**
   * Finds a move that is in the direction of stacking off to the side.
+  * currentDirection: the most recent direction moved by the snake.
   * grid: defined as in snakeAI.js
+  * startX, startY: The head of the snake
+  * endX, endY: The food location
   * returns: 0..3 depending on where the first move towards stacking off to the side is. View snakeAi.js for defintions of 0..3.
   */
 function stackOff(currentDirection, grid, startX, startY, endX, endY) {
