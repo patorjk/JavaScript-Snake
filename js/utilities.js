44a581138ce19e56e6dbfc8154e7257c5cffdffc
Matthew Page <matthewpagecs@gmail.com>
2015-10-24 13:14:14 -0400

    documentation

--- js/utilities.js
+++ js/utilities.js
@@ -1,11 +1,4 @@
-/**
-  * Finds the shortest path from start to end using the A-Star algorithm.
-  * grid: defined as in snakeAI.js
-  * startX, startY: The head of the snake
-  * endX, endY: The food location
-  * returns: 0..3 depending on where the first move towards the goal from the start is. View snakeAI.js for what 0..3 correspond to.
-  */
-
+// Returns a point object, given where it is in the grid and what its value is.
  function pointOfGrid(endX, endY, pValue) {
  	var toReturn = {
  		x: endX,
@@ -16,6 +9,7 @@
  	return toReturn;
  }
 
+// Returns a list of adjacent points, given a grid of Point objects, and a point object
 function getAdjacent(gridPoints,point) {
 	try {
 		var adjTiles = [];
@@ -42,6 +36,7 @@ function getAdjacent(gridPoints,point) {
 
 }
 
+// g, h, f are used in astar
 function g(obj) {
 	return obj.list.length -1;
 }
@@ -53,18 +48,24 @@ function h(obj,endX,endY) {
 function f(obj,endX,endY) {
 	return g(obj) + h(obj,endX,endY);
 }
+
+/**
+  * Finds the shortest path from start to end using the A-Star algorithm.
+  * grid: defined as in snakeAI.js
+  * startX, startY: The head of the snake
+  * endX, endY: The food location
+  * snakeBody: A circular linked list of the snake body. View snakeAI.js for more info
+  * returns: 0..3 depending on where the first move towards the goal from the start is. View snakeAI.js for what 0..3 correspond to.
+  */
   
-function astar(grid, startX, startY, endX, endY) {
-	// TODO
+function astar(grid, startX, startY, endX, endY, snakeBody) {
 	var gridPoints = [];
 	for (var i = 0; i < grid.length; i++) {
 		gridPoints.push([]);
 		for(var j = 0; j < grid.length; j++){
 			gridPoints[i].push(pointOfGrid(i,j, grid[i][j]));
 		}
-
 	}
-
 	var first = {
 		point:gridPoints[startX][startY],
 		list:[gridPoints[startX][startY]],
@@ -107,7 +108,6 @@ function astar(grid, startX, startY, endX, endY) {
 				};
 				processingList.push(addTo);
 			}
-
 		}
 	}
 	console.log("A* has been executed and has been unable to find a path.");
