ca3dce0b3c2982b9b3e3a82bd7a2d0498bd93c2d
Joseph Keller <josephkeller@fb.com>
2018-03-08 08:39:59 -0800

    first :art: updates

--- js/snake.js
+++ js/snake.js
@@ -826,7 +826,7 @@ SNAKE.Board = SNAKE.Board || (function() {
                 cLeft = 0;
                 cWidth = getClientWidth()-5;
                 cHeight = getClientHeight()-5;
-                document.body.style.backgroundColor = "#FC5454";
+                document.body.style.backgroundColor = "#FFFFFF";
             } else {
                 cTop = config.top;
                 cLeft = config.left;
