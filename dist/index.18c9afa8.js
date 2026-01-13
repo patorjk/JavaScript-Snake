58fce4b58ed868690bde965edbc7139e03a107db
VihaanD1234 <vihaan.dalal29@student.tamdistrict.org>
2026-01-13 16:45:48 +0000

    Start2

---
+++ dist/index.18c9afa8.js
@@ -0,0 +1,23 @@
+const mySnakeBoard = new SNAKE.Board({
+    boardContainer: "game-area",
+    fullScreen: true,
+    premoveOnPause: false,
+    onLengthUpdate: (length)=>{
+        console.log(`Length: ${length}`);
+    },
+    onPauseToggle: (isPaused)=>{
+        console.log(`Is paused: ${isPaused}`);
+    },
+    onInit: (params)=>{
+        console.log("init!");
+        console.log(params);
+    },
+    onWin: ()=>{
+        console.log("wn!");
+    },
+    onDeath: ()=>{
+        console.log("dead!");
+    }
+});
+
+//# sourceMappingURL=index.18c9afa8.js.map
